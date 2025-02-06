c7f80c4b552a472752eaaaed4e4524a92a962aff
Ihsan Saif <ihsansaifedwardion@gmail.com>
2025-02-06 17:33:52 +0500

    Basic components implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 export default function App()
 {
   return(
-    <Wel/>
+    <div className="app">
+    <Logo/>
+    <Form/>
+    <PackinList/>
+    <Stats/>
+    </div>
   )
 
 }
-function Wel()
+function Logo()
 {
   return(
-  <p>merging branch</p>
+  <h1>🧳FAR AWAY💼</h1>
+)
+}
+function Form()
+{
+  return(
+<div className="add-form">
+ <h3>What do you need for trip? </h3>
+</div>
+  )
+}
+function PackinList()
+{
+  return(
+  <div className="list">List</div>
+  )
+}
+function Stats()
+{
+return(
+  <footer className="stats">
+    <em>You have X items on your list and you already have Packed X (x%)</em>
+  </footer>
 )
 }
